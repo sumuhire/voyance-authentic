31cd1a9cb7d59b26b4aea9700d20c4984131354d
sumuhire <steveumuhire@gmail.com>
2019-07-30 10:39:49 +0200

    last changes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,62 +12,13 @@ class DefaultController extends Controller{
         );
     }
 
-    public function pressMenu(){
+    public function reviews(){
 
         return $this->render(
-            'Press/pressMenu.html.twig'
+            'Reviews/reviews.html.twig'
         );
     }
 
-    public function about(){
-
-        return $this->render(
-            'Default/about.html.twig'
-        );
-    }
-
-
-    public function produit(){
-
-        return $this->render(
-            'Produits/produit.html.twig'
-        );
-    }
-
-    public function dossier(){
-
-        return $this->render(
-            'Dossier/dossier.html.twig'
-        );
-    }
-
-    public function dossiers(){
-
-        return $this->render(
-            'Dossier/dossiers.html.twig'
-        );
-    }
-
-    public function panier(){
-
-        return $this->render(
-            'Panier/panier.html.twig'
-        );
-    }
-
-    public function inscription(){
-
-        return $this->render(
-            'Default/inscription.html.twig'
-        );
-    }
-
-    public function commande(){
-
-        return $this->render(
-            'Panier/commande.html.twig'
-        );
-    }
 
 
 }
